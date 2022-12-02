e927b95e1b5f0c83fa8b5a5c46097a23325be28b
caronf <florimond.caron@etu.umontpellier.fr>
2022-12-02 07:45:30 +0100

    fix XSS

--- Site/src/View/user/qCard.php
+++ Site/src/View/user/qCard.php
@@ -11,27 +11,27 @@
     <div class="container_QCard">
         <div class="container_section">
             <h4>Scène <?php echo $lvl;?></h4>
-            <p><?php echo $question1->get('texteQuestion'); ?></p>
+            <p><?php echo htmlspecialchars($question1->get('texteQuestion')) ; ?></p>
             </p>
         </div>
 
         <div class="container_answer">
             <div class="div_answer">
                 <a href="index.php?action=qCard&lvl=<?php echo $lvl;?>&confirm&id=<?php echo $reponse1->get('idReponse'); ?>" class="button">
-                <p><?php echo $reponse1->get('texteReponse'); ?>
+                <p><?php echo htmlspecialchars($reponse1->get('texteReponse')); ?>
                 </p>
                 </a>
             </div>
 
             <div class="div_answer">
                 <a href="index.php?action=qCard&lvl=<?php echo $lvl;?>&confirm&id=<?php echo $reponse2->get('idReponse'); ?>" class="button">
-                <p><?php echo $reponse2->get('texteReponse'); ?>
+                <p><?php echo htmlspecialchars($reponse2->get('texteReponse')); ?>
                 </p>
                 </a>
             </div>
             <div class="div_answer">
                 <a href="index.php?action=qCard&confirm&lvl=<?php echo $lvl;?>&id=<?php echo $reponse3->get('idReponse'); ?>" class="button">
-                <p><?php echo $reponse3->get('texteReponse'); ?>
+                <p><?php echo htmlspecialchars($reponse3->get('texteReponse')); ?>
                 </p>
                 </a>
             </div>
